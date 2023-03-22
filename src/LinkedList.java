c85dc142e37c3bf880da75854fc38535dae62bf0
sumithanvat <skhanvat@gmail.com>
2023-03-22 22:27:13 +0530

    Added:Lets create a

---
+++ src/LinkedList.java
@@ -0,0 +1,29 @@
+public class LinkedList {
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addNode(56);
+        list.addNode(30);
+        list.addNode(70);
+        list.printList();
+    }
+}
